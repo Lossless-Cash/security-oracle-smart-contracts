7cef7846625bb0ce7d80af569dde09ae5c1421c0
Ignacio Freire <nachofreire@gmail.com>
2022-04-25 11:04:30 -0300

    refactor: remove subNo logic and simplify withdraw

--- contracts/interfaces/ILosslessSecurityOracle.sol
+++ contracts/interfaces/ILosslessSecurityOracle.sol
@@ -24,6 +24,6 @@ interface ILssSecurityOracle {
     event NewSubscriptionExtension(address indexed _address, uint256 indexed _blocks);
     event NewWithdrawal(uint256 indexed _withdrawPool);
     event NewOracle(address indexed _oracle);
-    event NewOracleRemoved(address indexed _oracle);
+    event NewOracleRemoval(address indexed _oracle);
     event NewRiskScore(address indexed _updatedAddress, uint8 indexed _updatedScore);
 }
